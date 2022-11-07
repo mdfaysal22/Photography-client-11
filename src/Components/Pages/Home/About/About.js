2584a8b9ba8e6db58223bdf900b0c4b41bdf41f2
Md Faysal <mdfaysal.freelancer@gmail.com>
2022-11-08 01:07:10 +0600

    Service Page Design

---
+++ src/Components/Pages/Home/About/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className='my-5'>
+            <div className='flex justify-center flex-col md:flex-row items-center gap-10'>
+                <div className='w-full md:w-1/2'>
+                    
+                </div>
+                <div className='w-full md:w-1/2'>
+
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default About;
--- src/Components/Pages/Home/Services/Services.js
+++ src/Components/Pages/Home/Services/Services.js
@@ -10,13 +10,23 @@ const Services = () => {
   }, []);
   return (
     <div>
-        <div>
-            <h1 className="text-3xl ">Services</h1>
-        </div>
-      <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        {services.map((service) => (
+      <div className="text-center mt-10 mb-5">
+        <h1 className="text-3xl ">Services</h1>
+        <h3 className="lg:w-1/2 lg:mx-auto">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+          perferendis nostrum quaerat asperiores quia commodi suscipit natus
+          ducimus minus similique!
+        </h3>
+      </div>
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {services.slice(0, 3).map((service) => (
           <ServiceItem key={service._id} service={service}></ServiceItem>
         ))}
+      </div>
+      <div className="text-center my-5">
+        <button className="btn btn-outline rounded-none border-2">
+          All Service
+        </button>
       </div>
     </div>
   );
